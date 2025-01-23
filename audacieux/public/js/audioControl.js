15bdd23343658ef5357b7b12147b40b9c241b2dc
TomRintz <150177129+TomRintz@users.noreply.github.com>
2025-01-23 16:50:15 +0100

    Background sound

---
+++ audacieux/public/js/audioControl.js
@@ -0,0 +1,6 @@
+// js/audioControl.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    var audio = document.getElementById("background-audio");
+    audio.volume = 1.0; // Réglez le volume entre 0.0 et 1.0
+  });
