07a0be7c557c1c6c51c07b8c024c03e3e3ef19e2
Axel Riou <axelriou@etu.univ-nantes.fr>
2025-01-23 15:31:38 +0100

    Classe objet créée pour la quête (liste de course du joueur) et classe joueur pour la gestion de l'argent et achet d'objet

--- audacieux/public/utile.js
+++ audacieux/public/utile.js
@@ -1,3 +1,45 @@
+class Object {
+    constructor(name, value) {
+        this.name = name;
+        this.value = value;
+        this.isBuy = false;
+    }
+}
+
+
+class Player {
+    constructor(monney=0, shopping_list=[]) {
+        this.monney = monney;
+        this.shopping_list = shopping_list;
+    }
+
+    withdrawMonney(amount) {
+        if (amount > 0) {
+            this.monney += amount
+        }
+        else console.error("Montant invalide");
+    }
+
+    buyObject(obj) {
+        if (obj.value > this.monney) {
+            console.error("Solde insuffisant")
+        }
+        else {
+            this.monney -= obj.value;
+            this.shopping_list.push(obj);
+            obj.isBuy = true;
+        }
+    }
+
+    displayInventory() {
+        console.log("Argent restant: " + this.monney);
+        console.log("Liste de courses: ");
+        this.shopping_list.forEach(obj => console.log("- " + obj.name + ", " + obj.isBuy));
+    }
+
+}
+
+
 function colorizeAllImages(selector) {
     const images = document.querySelectorAll(selector);
 
@@ -37,6 +79,21 @@ function colorizeImage(imageElement) {
     });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+
+document.addEventListener("DOMContentLoaded", event => {
     colorizeAllImages(".interactive-image");
-});
+})
+
+const pringles = new Object("Pringles", 500);
+const sponge = new Object("Éponge", 300);
+const glass = new Object("Verre", 200);
+const latex_gloves = new Object("Gants en latex", 1000);
+const player = new Player(2000, [pringles, sponge, glass, latex_gloves]);
+
+
+player.buyObject(pringles);
+player.displayInventory();
+player.buyObject(sponge);
+player.buyObject(glass);
+player.buyObject(latex_gloves);
+player.displayInventory();
